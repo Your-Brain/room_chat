523791e93d6e7c810bd38db3d2bf98da88b47c05
Your_Brain <parthabatabyal156@gmail.com>
2025-03-02 17:38:49 +0530

    small change in ui

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
